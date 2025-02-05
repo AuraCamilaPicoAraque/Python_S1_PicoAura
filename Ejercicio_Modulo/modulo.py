92f34dc87c2113a776546ca974ccff86e21b34e5
AuraCamilaPicoAraque <auroraprogamer@gmail.com>
2025-02-05 01:01:58 -0500

    modulos

---
+++ Ejercicio_Modulo/modulo.py
@@ -0,0 +1,145 @@
+## Ejercicio - MODULOS [Aura Pico] cc: 1097498480 👨‍🦯
+
+from modulo import *
+
+nombres = [
+    ["Adrián"],
+    ["Alejandra"],
+    ["Ámbar", "Isabella"],
+    ["Andrés", "David"],
+    ["Aura", "Camila"],
+    ["Camilo", "Andrés"],
+    ["Daniel", "Esteban"],
+    ["David", "Santiago"],
+    ["Edgar", "Leonardo"],
+    ["Gerson", "Steven"],
+    ["Harley", "Yefrey"],
+    ["John", "Stiven"],
+    ["Juan", "David"],
+    ["Juan", "David"],
+    ["Juan", "David"],
+    ["Juan", "Eduardo"],
+    ["Juan", "Fernando"],
+    ["Juan", "Jose"],
+    ["Maria", "Juliana"],
+    ["Mateo", "Roman"],
+    ["Naya", "Zarela"],
+    ["Nicolas"],
+    ["Omar", "Fernando"],
+    ["Santiago"],
+    ["Santiago", "Andrés"],
+    ["Santiago"],
+    ["Sara", "Sofía"],
+    ["Sayara", "Yurley"],
+    ["Sergio", "Andrés"],
+    ["Simón", "Dante"],
+    ["Thomas", "Sebastián"],
+    ["Vladimir"]
+]
+
+apellidos = [
+    ["Quintero", "Pinzón"],
+    ["Pinzón", "Alvarez"],
+    ["Plata", "López"],
+    ["Reyes", "Espinel"],
+    ["Pico", "Araque"],
+    ["Suárez", "Fuentes"],
+    ["Guerrero", "Quintero"],
+    ["Vera", "Mendez"],
+    ["Acevedo", "Arteaga"],
+    ["Chaparro", "Martínez"],
+    ["Cabrales", "Vargas"],
+    ["Negron", "Regalado"],
+    ["Saavedra", "Jaimez"],
+    ["Santoyo", "Jaimes"],
+    ["Vargas", "Soto"],
+    ["Pinilla", "Guzmán"],
+    ["Umaña", "Barragan"],
+    ["Abril", "Roman"],
+    ["Saavedra", "Mejia"],
+    ["Camargo"],
+    ["Lizcano", "Jaimes"],
+    ["Chedraui", "Mantilla"],
+    ["Granados", "Parra"],
+    ["Aguilar", "Vesga"],
+    ["Quiñonez", "Sosa"],
+    ["Jaimes", "Perez"],
+    ["Díaz", "Rodríguez"],
+    ["Aparicio", "Arciniegas"],
+    ["Rueda", "Hernández"],
+    ["Salamanca", "Galvis"],
+    ["Bastos", "Garcia"],
+    ["Diaz", "Contreras"]
+]
+
+
+def agregar ():
+        n1=input("ingrese el primer nombre del estudiante")
+        n2=input("ingrese el segundo nombre (si lo tiene)")
+
+        ap1=input("Ingrese el primer apellido")
+        ap2=input("ingrese el segundo apellido")
+
+        nombres.append([n1,n2])
+        apellidos.append([ap1,ap2])
+
+def ver ():
+        
+        c1 = 0
+        c = 0
+
+        for i in range(len(nombres)):
+            ##nombres = [["Adrián"]] ---- apellidos = [["Quintero", "Pinzón"]] --- Adrían Quintero Pinzón
+            print("estudiante#",i+1," "," ".join(nombres[c1])," ".join(apellidos[c]))
+            c1+=1
+            c+=1
+
+def edit ():
+            
+            ver ()
+
+            edt=int(input("Ingrese el estudiante que quiere editar (el numero de la lista):  "))
+            if 0 <= edt < len(nombres):
+
+                print("Seleccione qué desea editar:  ")
+                print("1. Primer nombre\n2. Segundo nombre\n3. Primer apellido\n4. Segundo apellido")
+
+                opc = int (input(": "))
+                opciones = input ("Ingresa el nuevo nombre o apellido:  ")
+
+## Aqui editamos el nombre....
+                if opc == 1 :
+                      nombres[edt-1][0]=(opciones)
+
+## Porque pongo "and len(nombres[edt])>1 :"  esto es para comprobar en la lista si el estudiante tiene un segundo nombre si no es asi te vota error 
+                elif opc == 2 and len(nombres[edt])>1 :
+                      nombres[edt-1][1]= (opciones)     
+
+## Y aqui editamos el apellido....
+                elif opc == 3 :
+                       apellidos[edt-1][0]= (opciones)   
+
+                elif opc == 4 :
+                      apellidos[edt-1][1]= (opciones)   
+
+
+
+                else:
+                       print("La opcion que pusiste no esta disponible ~~")
+            else: 
+                   print("Numero de estudiante no encontrado ~~~")
+   
+def eli ():
+            
+            ver ()
+            el=int(input("ingrese el estudiante que quiere eliminar (el numero de la lista):  "))
+            nombres.pop(el-1)
+            apellidos.pop(el-1)
+
+            print("Estudiante eliminado exitosamente ;) ~~~")
+       
+def exit ():
+        print("~~~~~~ Saliendo de programa ~~~~~~~")
+        
+
+## Ejercicio - MODULOS [Aura Pico] cc: 1097498480 👨‍🦯
---
+++ Ejercicio_Modulo/programa.py
@@ -0,0 +1,40 @@
+## Ejercicio - MODULOS [Aura Pico] cc: 1097498480 👨‍🦯
+
+import modulo
+## Sirve para que tengan una coorelacion el modulo.py y yo pueda utilizar las varibles de modulo.(nombre) () 
+
+booleanito = True
+while booleanito ==True:
+
+    print("\nBienvenido al programa de lista de estudiantes")
+    print("Que te gustaría hacer?")
+    print("1.Agregar estudiante")
+    print("2. Ver estudiantes")
+    print("3.Editar estudiante")
+    print("4.Eliminar estudiante")
+    print("5.Salir del programa")
+    
+    opc = int(input(":"))
+
+
+    if opc == 1:
+        modulo.agregar()
+
+    elif opc == 2 :
+         modulo.ver ()
+
+    elif opc == 3 :
+         modulo.edit ()
+        
+    elif opc == 4 :
+         modulo.eli ()
+     
+    elif opc == 5 :
+        modulo.exit ()
+        break
+    
+    else:
+        print("Opción incorrecta, intenta de nuevo.")
+         
+         
+## Ejercicio - MODULOS [Aura Pico] cc: 1097498480 👨‍🦯
